b57e526be920d19dc350b616d03f4f2e788c2d01
seongjin <77876601+ysjkof@users.noreply.github.com>
2021-09-07 21:45:22 +0900

    WIP

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import AppRouter from "./Router";
+import { authService } from "../fbase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return <AppRouter isLoggedIn={isLoggedIn} />;
+}
+
+export default App;
---
+++ src/routes/Auth.js
@@ -0,0 +1,66 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "@firebase/auth";
+import { useState } from "react";
+import { authService } from "../fbase";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = createUserWithEmailAndPassword(authService, email, password);
+      } else {
+        data = signInWithEmailAndPassword(authService, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
